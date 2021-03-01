c968edfc127a1d142140a7dc06e59c0f8018be60
Andrew Fahmy <andrewj.fahmy@gmail.com>
2021-03-01 11:47:08 -0600

    Added simple install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+CONFIG_DIR=$XDG_CONFIG_HOME
+
+if [ -z ${CONFIG_DIR} ]
+then
+    echo "XDG_CONFIG_HOME not defined, using ~/.config"
+    CONFIG_DIR=~/.config
+fi
+
+ln -siv $DOTFILES_DIR/config/* $CONFIG_DIR/
