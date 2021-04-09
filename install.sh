22aa0eff022dd88b7d2998f33dd98adca490c65c
Andrew Fahmy <andrewj.fahmy@gmail.com>
2021-04-09 10:58:28 -0500

    adding .zshenv to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -11,3 +11,4 @@ then
 fi
 
 ln -siv $DOTFILES_DIR/config/* $CONFIG_DIR/
+ln -siv $DOTFILES_DIR/.zshenv $HOME
