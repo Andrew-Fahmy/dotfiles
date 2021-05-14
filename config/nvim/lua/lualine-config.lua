87705a895b09611ad905cd80619006d167098d1d
Andrew Fahmy <andrewj.fahmy@gmail.com>
2021-05-13 22:42:07 -0500

    Changing lualine icons

--- config/nvim/lua/lualine-config.lua
+++ config/nvim/lua/lualine-config.lua
@@ -1,12 +1,17 @@
 require'lualine'.setup{
     options = {
+        section_separators = {'', ''},
+        component_separators = {'┃', '┃'},
+        --section_separators = {'', ''},
+        --component_separators = {'', ''},
         theme = 'tokyonight'
     },
     sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch' },
+        lualine_b = {{ 'branch' }},
+        --lualine_b = {{ 'branch', icon = '' }},
         lualine_c = { 'filename' },
-        lualine_x = {{ 'filetype', icons_enabled = false }},
+        lualine_x = { 'filetype' },
         lualine_y = { 'encoding', { 'fileformat', icons_enabled = false }},
         lualine_z = { 'location', 'progress' }
     },
