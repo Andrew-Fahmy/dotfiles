b11f4b5a6a85ffc42c9ad4386fbcb6c2e3cc540c
Andrew Fahmy <andrewj.fahmy@gmail.com>
2024-02-21 11:13:55 -0600

    update tmux config

--- config/tmux/work.sh
+++ config/tmux/work.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 if ! tmux switch-client -t work 2> /dev/null; then
-    tmux new-session -d -s work -c ~/work/tpb
+    tmux new-session -d -s work -c ~/Vas/
     tmux switch-client -t work
-    tmux send-keys "vim ." Enter
-    tmux rename-window "editor"
+
+    tmux send-keys "cd vas-sls-integrations/serverless/" Enter
+    tmux send-keys "nvim" Enter
+    tmux rename-window "vas-sls-integrations"
+
+    tmux new-window -c ~/Vas/
+    tmux send-keys "cd vas-api-developer/src/serverless/" Enter
+    tmux send-keys "nvim" Enter
+    tmux rename-window "vas-api-developer"
+
+    tmux new-window -c ~/Vas/
+    tmux send-keys "cd vas-sdk-typescript/src/packages/" Enter
+    tmux send-keys "nvim" Enter
+    tmux rename-window "vas-sdk-typescript"
 fi
